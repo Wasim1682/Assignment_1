b7dc5020e74356ecd3eb71e055f21c6392b52344
Wasim1682 <154121710+Wasim1682@users.noreply.github.com>
2025-11-23 09:55:48 +0530

    Update TASK_2.py

--- TASK_2.py
+++ TASK_2.py
@@ -1,3 +1,4 @@
-a=input("Enter your first name: ")
-b=input("Enter your last name: ")
+a=str(input("Enter your first name: "))
+b=str(input("Enter your last name: "))
+
 print("Hello",a,b,"! Welcome to Python")
